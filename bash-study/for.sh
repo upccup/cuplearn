8fc30f561e464165d57f34fac5031ce797d05750
upccup <yaoyunsmile@163.com>
2015-11-09 13:36:37 +0800

    update while.sh

--- bash-study/for.sh
+++ bash-study/for.sh
@@ -14,5 +14,21 @@ check_omega_agent() {
 	   fi
 }
 
+
+check_omega_agent_a() {
+  if ps ax | grep -v grep | grep "omega-agent" > /dev/null
+	  then
+	    echo "Omega Agent service is running now... "
+	    echo "Wraning!!! Continue installation will overwrite the original version"
+	    install_wait=10
+	        while [ $install_wait -gt 0 ]
+	        do
+	           echo "new omega-agent will install after ${install_wait}s" 
+	           install_wait=`expr $install_wait - 1`
+	           sleep 1s
+	        done
+	fi
+}
+
 check_omega_agent
                        
