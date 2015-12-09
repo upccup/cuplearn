86e8c72b6797b00cf245ee63599cbece32db3a57
upccup <yaoyunsmile@163.com>
2015-12-09 22:34:55 +0800

    update result

--- go-study/time/add.go
+++ go-study/time/add.go
@@ -12,5 +12,7 @@ func main() {
 	now := time.Now()
 	fmt.Println("now", now)
 	fmt.Println("ten minute after: ", now.Add(time.Minute*10))
+	// ten minute after:  2015-12-09 22:44:15.140701673 +0800 CST
 	fmt.Println("ten minute ago: ", now.Add(time.Minute*-10))
+	// ten minute ago:  2015-12-09 22:24:15.140701673 +0800 CST
 }
--- go-study/time/compare.go
+++ go-study/time/compare.go
@@ -13,5 +13,7 @@ func main() {
 	befroe := now.Add(-1 * time.Minute)
 
 	fmt.Println(befroe, "is bfefroe ", after, " ?", befroe.Before(after))
+	// 2015-12-09 22:31:25.361393411 +0800 CST is bfefroe  2015-12-09 22:33:25.361393411 +0800 CST  ? true
 	fmt.Println(befroe, "is after ", after, " ?", befroe.After(after))
+	// 2015-12-09 22:31:25.361393411 +0800 CST is after  2015-12-09 22:33:25.361393411 +0800 CST  ? false
 }
