8b967fddc7584b820da2eb5a775e79f06ee350f3
upccup <yaoyunsmile@163.com>
2016-03-15 11:25:54 +0800

    add tar adn untar

--- go-study/file/tar.go
+++ go-study/file/tar.go
@@ -2,45 +2,79 @@ package main
 
 import (
 	"archive/tar"
+	"compress/gzip"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
-func main() {
-	var tarFile, filePath string
-	tarFile = "/data/test.tar"
-	filePath = "./"
+func writeFileToTar(writer *tar.Writer, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-	f, err := os.Open(tarFile)
+	stat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	header := &tar.Header{
+		Name:    sanitizedName(filename),
+		Mode:    int64(stat.Mode()),
+		Uid:     os.Getuid(),
+		Gid:     os.Getgid(),
+		Size:    stat.Size(),
+		ModTime: stat.ModTime(),
+	}
+	if err = writer.WriteHeader(header); err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, file)
+	return err
+}
+
+func sanitizedName(fileName string) string {
+	if len(fileName) > 1 && fileName[1] == ':' && runtime.GOOS == "windows" {
+		fileName = fileName[2:]
+	}
+
+	fileName = filepath.ToSlash(fileName)
+	fileName = strings.TrimLeft(fileName, "/.")
+
+	return strings.Replace(fileName, "../", "", -1)
+}
+
+func main() {
+	var tarFileName string = "test.tar"
+	var files []string = []string{"file1.txt", "file2.txt"}
+
+	file, err := os.Create(tarFileName)
 	if err != nil {
 		log.Panicln(err)
 	}
-	defer f.Close()
+	defer file.Close()
 
-	tr := tar.NewReader(f)
-	for {
-		hdr, err := tr.Next()
-		if err == io.EOF {
-			break
-		} else if err != nil {
+	var fileWriter io.WriteCloser = file
+
+	if strings.HasSuffix(tarFileName, ".gz") {
+		fileWriter = gzip.NewWriter(file)
+		defer fileWriter.Close()
+	}
+
+	writer := tar.NewWriter(fileWriter)
+	defer writer.Close()
+
+	for _, name := range files {
+		if err := writeFileToTar(writer, name); err != nil {
 			log.Panicln(err)
 		}
-
-		if hdr.FileInfo().IsDir() {
-			os.Mkdir(filePath+string(os.PathSeparator)+hdr.Name, hdr.FileInfo().Mode())
-		} else {
-			func() {
-				fw, err := os.OpenFile(filePath+string(os.PathSeparator)+hdr.Name, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, hdr.FileInfo().Mode())
-				if err != nil {
-					log.Panicln(err)
-				}
-				defer fw.Close()
-				_, err = io.Copy(fw, tr)
-				if err != nil {
-					log.Panicln(err)
-				}
-			}()
-		}
 	}
+
+	return
 }
