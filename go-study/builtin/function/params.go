64d372b8a93251f465d08e1e0fc0bf37dcb68fdc
upccup <yaoyunsmile@163.com>
2016-10-31 11:14:32 +0800

    add multi agrs example

--- go-study/builtin/function/params.go
+++ go-study/builtin/function/params.go
@@ -14,6 +14,10 @@ func add2(x *int) int {
 	return *x
 }
 
+func multiArgs(name string, books ...string) {
+	log.Printf("name: %s books: %+v", name, books)
+}
+
 func main() {
 	var x = 1
 
@@ -26,13 +30,23 @@ func main() {
 	x2 := add2(&x)
 	log.Println("x+2 = ", x2)
 	log.Println("x = ", x)
+
+	multiArgs("Aaaaa")
+	multiArgs("Bbbbb", "C")
+	multiArgs("Ccccc", "C", "C++")
+	books := []string{"C", "C++", "C#"}
+	multiArgs("Ddddd", books...)
 }
 
 // out put
 /*
-x =  1
-x+1 =  2
-x =  1
-x+2 =  3
-x =  3
+2016/10/31 11:13:00 x =  1
+2016/10/31 11:13:00 x+1 =  2
+2016/10/31 11:13:00 x =  1
+2016/10/31 11:13:00 x+2 =  3
+2016/10/31 11:13:00 x =  3
+2016/10/31 11:13:00 name: Aaaaa books: []
+2016/10/31 11:13:00 name: Bbbbb books: [C]
+2016/10/31 11:13:00 name: Ccccc books: [C C++]
+2016/10/31 11:13:00 name: Ddddd books: [C C++ C#]
 */
