e37cf2bef598739280387d499b49f829c535d4f3
upccup <yaoyunsmile@163.com>
2016-12-01 20:18:01 +0800

    update request about reader

--- go-study/httpServer.go
+++ go-study/httpServer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -17,7 +19,9 @@ func main() {
 	{
 		// v1.POST("/login", loginEndpoint)
 		v1.POST("/submit", submitEndpoint)
-		// v1.POST("/read", readEndpoint)
+		v1.POST("/test", testEndpoint)
+		v1.POST("/test2", testEndpoint2)
+		v1.POST("/test3", testEndpoint3)
 	}
 
 	// Simple group: v2
@@ -25,7 +29,7 @@ func main() {
 	{
 		// v2.POST("/login", loginEndpoint)
 		v2.GET("/submit", submitEndpoint)
-		// v2.POST("/read", readEndpoint)
+		v2.POST("/test", testEndpoint)
 	}
 
 	router.Run(":8080")
@@ -36,7 +40,10 @@ func submitEndpoint(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	defer body.Close()
 	x, _ := ioutil.ReadAll(body)
-	fmt.Printf("%s \n", string(x))
+	fmt.Printf("X: %s \n", string(x))
+
+	y, _ := ioutil.ReadAll(body)
+	fmt.Printf("Y: %s \n", string(y))
 
 	// c.Next()
 	// if c.Bind(&test) == nil {
@@ -45,4 +52,64 @@ func submitEndpoint(c *gin.Context) {
 	// 	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	// }
 
+}
+
+func testEndpoint(c *gin.Context) {
+	body := c.Request.Body
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	defer body.Close()
+
+	var bodyBytes []byte
+
+	if c.Request.Body != nil {
+		bodyBytes, _ = ioutil.ReadAll(body)
+	}
+
+	//body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	fmt.Printf("X: %s \n", string(bodyBytes))
+
+	y, _ := ioutil.ReadAll(c.Request.Body)
+	fmt.Printf("Y: %s \n", string(y))
+}
+
+func testEndpoint2(c *gin.Context) {
+	body := c.Request.Body
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	defer body.Close()
+
+	var buf bytes.Buffer
+	tee := io.TeeReader(body, &buf)
+
+	y, _ := ioutil.ReadAll(tee)
+	fmt.Printf("Y: %s \n", string(y))
+
+	x, _ := ioutil.ReadAll(&buf)
+	fmt.Printf("X: %s \n", string(x))
+
+	z, _ := ioutil.ReadAll(body)
+	fmt.Printf("Z: %s \n", string(z))
+}
+
+// Output
+// Y: aaaaaaaa
+// X: aaaaaaaa
+// Z:
+
+func testEndpoint3(c *gin.Context) {
+	body := c.Request.Body
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	defer body.Close()
+
+	var buf bytes.Buffer
+	tee := io.TeeReader(body, &buf)
+
+	x, _ := ioutil.ReadAll(&buf)
+	fmt.Printf("X: %s \n", string(x))
+
+	y, _ := ioutil.ReadAll(tee)
+	fmt.Printf("Y: %s \n", string(y))
+
+	z, _ := ioutil.ReadAll(body)
+	fmt.Printf("Z: %s \n", string(z))
 }
