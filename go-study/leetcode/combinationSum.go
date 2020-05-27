f0ecca515dfa3b46ddc3fb777075ad36a905bd7d
姚云 <yy152986@alibaba-inc.com>
2020-05-27 14:14:28 +0800

    add  combinationSumII

--- go-study/leetcode/combinationSum.go
+++ go-study/leetcode/combinationSum.go
@@ -1,14 +1,14 @@
 package leetcode
 
 import (
+	"reflect"
 	"sort"
 )
 
 var result [][]int
 
+// https://leetcode.com/problems/combination-sum/
 func combinationSum(candidates []int, target int) [][]int {
-	sort.Ints(candidates)
-
 	return combine(0, target, []int{}, candidates)
 }
 
@@ -19,12 +19,89 @@ func combine(sum int, target int, curComb []int, candidates []int) [][]int {
 		return [][]int{append([]int{}, curComb...)}
 	} else if sum < target {
 		for i, v := range candidates {
-			// sum = sum + v
-			// curComb = append(curComb, v)
-			// tmp = combine(sum, target, curComb, candidates[i:])
 			tmp = combine(sum+v, target, append(curComb, v), candidates[i:])
 			result = append(result, tmp...)
 		}
 	}
 	return result
+}
+
+// https://leetcode.com/problems/combination-sum-ii/
+func combinationSumII(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	return combineII(0, target, []int{}, candidates)
+}
+
+func combineII(sum int, target int, curComb []int, candidates []int) [][]int {
+	var tmp, result [][]int
+	if sum == target {
+		return [][]int{append([]int{}, curComb...)}
+	} else if sum < target {
+		for i, v := range candidates {
+			tmp = combineII(sum+v, target, append(curComb, v), candidates[i+1:])
+			result = merge(result, tmp)
+		}
+	}
+
+	return result
+}
+
+func merge(a, b [][]int) [][]int {
+	for _, v := range b {
+		if !contain(a, v) {
+			a = append(a, v)
+		}
+	}
+
+	return a
+}
+
+func contain(a [][]int, b []int) bool {
+	for _, v := range a {
+		if reflect.DeepEqual(v, b) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// https://leetcode.com/problems/combination-sum-ii/discuss/498085/Go-0ms
+func combinationSumIIOptimize(candidates []int, target int) [][]int {
+	result := [][]int{}
+	sort.Ints(candidates)
+	combineIIOptimize(candidates, target, []int{}, &result)
+	return result
+}
+
+func combineIIOptimize(candidates []int, target int, curComb []int, result *[][]int) {
+	if len(candidates) == 0 {
+		return
+	}
+
+	if candidates[0] == target {
+		curComb = append(curComb, candidates[0])
+		*result = append(*result, curComb)
+		return
+	} else if candidates[0] > target {
+		return
+	} else {
+		for i := 0; i < len(candidates)-1; i++ {
+			// 去重
+			if candidates[i] != candidates[i+1] {
+				combineIIOptimize(candidates[i+1:], target, curComb, result)
+				curComb2 := make([]int, len(curComb))
+				copy(curComb2, curComb)
+				curComb2 = append(curComb, candidates[0])
+				combineIIOptimize(candidates[1:], target-candidates[0], curComb2, result)
+				return
+			}
+		}
+
+		curComb3 := make([]int, len(curComb))
+		copy(curComb3, curComb)
+		curComb3 = append(curComb3, candidates[0])
+		combineIIOptimize(candidates[1:], target-candidates[0], curComb3, result)
+		return
+	}
 }
--- go-study/leetcode/combinationSum_test.go
+++ go-study/leetcode/combinationSum_test.go
@@ -6,9 +6,19 @@ import (
 )
 
 func TestCombinationSum(t *testing.T) {
-	// result := combinationSum([]int{2, 3, 6, 7}, 7)
-	// fmt.Println(result)
-	result := combinationSum([]int{2, 3, 5}, 8)
+	fmt.Println("TestCombinationSum: ")
+	result := combinationSum([]int{2, 3, 6, 7}, 7)
+	fmt.Println(result)
+	result = combinationSum([]int{2, 3, 5}, 8)
 	fmt.Println(result)
 	t.Log(result)
+}
+
+func TestCombinationSumII(t *testing.T) {
+	fmt.Println("TestCombinationSumII: ")
+	result := combinationSumII([]int{10, 1, 2, 7, 6, 1, 5}, 8)
+	fmt.Println(result)
+	fmt.Println("TestCombinationSumIIOptimize: ")
+	result = combinationSumIIOptimize([]int{10, 1, 2, 7, 6, 1, 5}, 8)
+	fmt.Println(result)
 }
